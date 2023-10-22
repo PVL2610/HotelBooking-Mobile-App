ab999ea5200e9650bf4a6cf213706590e532545f
Nhathuy <phamvannhathuy160903@gmail.com>
2023-10-23 00:52:55 +0700

    Code booking form p1

---
+++ app/src/main/java/com/example/hotelbooking_app/Booking/Activity/BookingActivity.java
@@ -0,0 +1,15 @@
+package com.example.hotelbooking_app.Booking.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hotelbooking_app.R;
+
+public class BookingActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.booking_layout);
+    }
+}
